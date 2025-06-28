60f510118cbe9fd6f33fbfb867c7c156e710ccb1
wellsonhuang <wellsonhuang888@gmail.com>
2025-06-28 11:16:04 -0400

    first

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,55 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+      const price = parseFloat(item.cost.substring(1));
+      total += price * item.quantity;
+    });
+    return total.toFixed(2);
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping(e);  // 呼叫 parent 提供的方法
   };
 
 
 
   const handleIncrement = (item) => {
-  };
+  dispatch(updateQuantity({
+    name: item.name,
+    quantity: item.quantity + 1,
+  }));
+};
+
 
   const handleDecrement = (item) => {
-   
-  };
+  if (item.quantity > 1) {
+    dispatch(updateQuantity({
+      name: item.name,
+      quantity: item.quantity - 1,
+    }));
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
-  const handleRemove = (item) => {
+
+const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
+  
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return (parseFloat(item.cost.substring(1)) * item.quantity).toFixed(2);
   };
+  
 
   return (
     <div className="cart-container">
